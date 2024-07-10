976feabc8a6369379b73121b789ffa2ad135ecaf
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-11 02:23:53 +0700

    build : building base projects

---
+++ src/Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,5 @@
+namespace Domain.Primitives;
+public abstract class AggregateRoot
+{
+
+}
---
+++ src/Infrastructure/VitomDBContext.cs
@@ -0,0 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+public class VitomDBContext() : DbContext()
+{
+
+}
