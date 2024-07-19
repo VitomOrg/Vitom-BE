fb21380cf58e4bd80e5a60aa38c88f815c8c39e1
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-19 22:57:22 +0700

    delete all template user use cases

--- src/API/Endpoints/UserEndpoints.cs
+++ src/API/Endpoints/UserEndpoints.cs
@@ -1,13 +1,9 @@
-using API.EndpointHandlers.UserEndpointHandler;
-using Swashbuckle.AspNetCore.Annotations;
-
 namespace API.Endpoints;
 
 public static class UserEndpoints
 {
     public static RouteGroupBuilder MapUserEndpoint(this RouteGroupBuilder group)
     {
-        group.MapGet("/", GetUserByIdEndpointHandler.Handle).WithMetadata(new SwaggerOperationAttribute("con cac"));
         return group;
     }
 }
