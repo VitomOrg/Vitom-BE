d0454e94de3f1bb75d1f228b84e56d568043ce95
PersieDaCoder <thinhdpham2510@gmail.com>
2024-08-27 10:01:13 +0700

    feat: update software

--- src/API/EndpointHandlers/SoftwareEndpointHandlers/CreateSoftwareEndpointHandler.cs
+++ src/API/EndpointHandlers/SoftwareEndpointHandlers/CreateSoftwareEndpointHandler.cs
@@ -1,6 +1,6 @@
 using API.Utils;
 using Application.Responses.SoftwareResponses;
-using Application.UC_Software;
+using Application.UC_Software.Commands;
 using Ardalis.Result;
 using MediatR;
 
@@ -10,7 +10,7 @@ public class CreateSoftwareEndpointHandler
 {
     public static async Task<Microsoft.AspNetCore.Http.IResult> Handle(ISender sender, CreateSoftwareRequest request, CancellationToken cancellationToken = default)
     {
-        Result<CreateSoftwareResponse> result = await sender.Send(new CreateSoftware.CreateSoftwareCommand(
+        Result<CreateSoftwareResponse> result = await sender.Send(new CreateSoftware.Command(
             Name: request.Name,
             Description: request.Description
         ), cancellationToken);
