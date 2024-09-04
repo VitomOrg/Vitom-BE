0a32da35d43baa3e7c97080969f53f6dd6728363
mglongbao <mglongbao@gmail.com>
2024-09-04 09:47:01 +0700

    fix: hot fix for retrieving type

--- src/API/Middlewares/MinimalAPIMiddleware.cs
+++ src/API/Middlewares/MinimalAPIMiddleware.cs
@@ -1,12 +1,14 @@
 using API.Endpoints;
 
 namespace API.Middlewares;
+
 public static class MinimalAPIMiddleware
 {
     public static WebApplication MapMinimalAPI(this WebApplication host)
     {
         host.MapGroup("users").MapUserEndpoint().WithTags("Users");
         host.MapGroup("softwares").MapSoftwareEndpoint().WithTags("Softwares");
+        host.MapGroup("types").MapTypeEndpoint().WithTags("Types");
         host.MapGroup("reviews").MapReviewEndpoint().WithTags("Reviews");
         host.MapGroup("products").MapProductEndpoint().WithTags("Products");
         return host;
