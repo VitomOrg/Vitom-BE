a682488000b9eb118309ce95db3448fc131ebcf0
mglongbao <mglongbao@gmail.com>
2024-09-17 14:39:46 +0700

    Change to IsNotDelete

--- src/Application/UC_Software/Queries/ViewListOfSoftware.cs
+++ src/Application/UC_Software/Queries/ViewListOfSoftware.cs
@@ -11,34 +11,39 @@ namespace Application.UC_Software.Queries;
 
 public class ViewListOfSoftware
 {
-    public record Query(
-        string Keyword,
-        int PageSize,
-        int PageIndex
-    ) : IRequest<Result<PaginatedResponse<SoftwareDetailsResponse>>>;
-    public class Handler(IVitomDbContext context, ICacheServices cacheServices) : IRequestHandler<Query, Result<PaginatedResponse<SoftwareDetailsResponse>>>
+    public record Query(string Keyword, int PageSize, int PageIndex)
+        : IRequest<Result<PaginatedResponse<SoftwareDetailsResponse>>>;
+
+    public class Handler(IVitomDbContext context, ICacheServices cacheServices)
+        : IRequestHandler<Query, Result<PaginatedResponse<SoftwareDetailsResponse>>>
     {
-        public async Task<Result<PaginatedResponse<SoftwareDetailsResponse>>> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<PaginatedResponse<SoftwareDetailsResponse>>> Handle(
+            Query request,
+            CancellationToken cancellationToken
+        )
         {
             // set key
-            string key = $"software-keyword{request.Keyword}-pagesize{request.PageSize}-pageindex-{request.PageIndex}";
+            string key =
+                $"software-keyword{request.Keyword}-pagesize{request.PageSize}-pageindex-{request.PageIndex}";
             // get cache result
-            PaginatedResponse<SoftwareDetailsResponse>? cacheResult = await cacheServices.GetAsync<PaginatedResponse<SoftwareDetailsResponse>>(key, cancellationToken);
-            if (cacheResult is not null) return Result.Success(cacheResult, "Get softwares Successfully");
+            PaginatedResponse<SoftwareDetailsResponse>? cacheResult = await cacheServices.GetAsync<
+                PaginatedResponse<SoftwareDetailsResponse>
+            >(key, cancellationToken);
+            if (cacheResult is not null)
+                return Result.Success(cacheResult, "Get softwares Successfully");
             // query
-            IQueryable<Software> query =
-                context.Softwares
-                    .AsNoTracking()
-                    .Where(s => s.Name.ToLower().Contains(request.Keyword.ToLower()))
-                    .Where(s => s.DeletedAt != null);
+            IQueryable<Software> query = context
+                .Softwares.AsNoTracking()
+                .Where(s => s.Name.ToLower().Contains(request.Keyword.ToLower()))
+                .Where(s => s.IsNotDeleted());
             // calculating total pages
             int totalPages = (int)Math.Ceiling((decimal)query.Count() / request.PageSize);
             // get result
-            IEnumerable<SoftwareDetailsResponse> result =
-                await query.Skip((request.PageIndex - 1) * request.PageSize)
-                    .Take(request.PageSize)
-                    .Select(s => s.MapToSoftwareDetailsResponse())
-                    .ToListAsync(cancellationToken);
+            IEnumerable<SoftwareDetailsResponse> result = await query
+                .Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(s => s.MapToSoftwareDetailsResponse())
+                .ToListAsync(cancellationToken);
             // map to paginated result
             cacheResult = new(
                 Data: result,
--- src/Domain/Primitives/Entity.cs
+++ src/Domain/Primitives/Entity.cs
@@ -9,20 +9,20 @@ public abstract class Entity
     public DateTimeOffset? UpdatedAt { get; set; }
     public DateTimeOffset? DeletedAt { get; set; }
 
-    public void Delete()
-        => DeletedAt = DateTimeOffset.UtcNow;
+    public bool IsDeleted() => DeletedAt != null;
 
-    public void Restore()
-        => DeletedAt = null!;
+    public bool IsNotDeleted() => DeletedAt == null;
+
+    public void Delete() => DeletedAt = DateTimeOffset.UtcNow;
+
+    public void Restore() => DeletedAt = null!;
 
     public List<BaseEvent> _domainEvents = [];
 
     [NotMapped]
     public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(BaseEvent domainEvent)
-        => _domainEvents.Add(domainEvent);
+    public void AddDomainEvent(BaseEvent domainEvent) => _domainEvents.Add(domainEvent);
 
-    public void ClearDomainEvents()
-        => _domainEvents.Clear();
+    public void ClearDomainEvents() => _domainEvents.Clear();
 }
