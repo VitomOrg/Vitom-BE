eed454734658c60a299c520d2f15132eb8900cb5
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-09-18 19:17:01 +0700

    fix message for create product

--- src/Application/UC_Product/Commands/CreateProduct.cs
+++ src/Application/UC_Product/Commands/CreateProduct.cs
@@ -47,14 +47,16 @@ public class CreateProduct
 
             //check product types are existed
             IEnumerable<Type> checkingTypes = context.Types.Where(t => request.TypeIds.Contains(t.Id) && t.DeletedAt == null);
-            if (checkingTypes.Count() != request.TypeIds.Length) return Result.NotFound($"Type with id {request.TypeIds} is not existed");
+            if (checkingTypes.Count() != request.TypeIds.Length) return Result.NotFound($"Types with id {request.TypeIds} are not existed");
             // add product types
             newProduct.ProductTypes = checkingTypes.Select(t => new ProductType { ProductId = newProduct.Id, TypeId = t.Id }).ToList();
+
             //check product softwares are existed
             IEnumerable<Software> checkingSoftwares = context.Softwares.Where(s => request.SoftwareIds.Contains(s.Id) && s.DeletedAt == null);
-            if (checkingSoftwares.Count() != request.SoftwareIds.Length) return Result.NotFound($"Software with id {request.SoftwareIds} is not existed");
+            if (checkingSoftwares.Count() != request.SoftwareIds.Length) return Result.NotFound($"Softwares with id {request.SoftwareIds} are not existed");
             // add product softwares
             newProduct.ProductSoftwares = checkingSoftwares.Select(s => new ProductSoftware { ProductId = newProduct.Id, SoftwareId = s.Id }).ToList();
+
             // add product images
             List<Task<string>> tasks = [];
             // upload images
