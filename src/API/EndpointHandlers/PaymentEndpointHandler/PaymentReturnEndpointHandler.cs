75ea2248131b3c3f1e92d5501fb1051027ca0dc9
mglongbao <mglongbao@gmail.com>
2024-09-23 18:19:17 +0700

    Feat: handle complete payment (fix later for redirect url)

---
+++ src/API/EndpointHandlers/PaymentEndpointHandler/PaymentReturnEndpointHandler.cs
@@ -0,0 +1,30 @@
+using Application.UC_Payment.Commands;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace API.EndpointHandlers.PaymentEndpointHandler;
+
+public class PaymentReturnEndpointHandler
+{
+    public static async Task<IResult> Handle(
+        [FromQuery] string code,
+        [FromQuery] string id,
+        [FromQuery] bool cancel,
+        [FromQuery] string status,
+        [FromQuery] int orderCode,
+        ISender sender,
+        CancellationToken cancellationToken = default
+    )
+    {
+        Result result = await sender.Send(
+            new ProcessPaymentReturn.Command(code, id, cancel, status, orderCode),
+            cancellationToken
+        );
+
+        return result.IsSuccess
+            ? Results.Redirect("/payment-success")
+            : Results.Redirect("/payment-failure");
+    }
+}
