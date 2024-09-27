189148227c551f7b69ce12c4f831d09f3b4e5878
PersieDaCoder <thinhdpham2510@gmail.com>
2024-09-28 00:59:50 +0700

    feat:delete collection

---
+++ src/Application/UC_Collection/Commands/DeleteCollection.cs
@@ -0,0 +1,32 @@
+using Application.Contracts;
+using Ardalis.Result;
+using Domain.Entities;
+using Domain.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UC_Collection.Commands;
+
+public class DeleteCollection
+{
+    public record Command(Guid Id) : IRequest<Result>;
+
+    public class Handler(IVitomDbContext context, CurrentUser currentUser) : IRequestHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            // check if user is admin
+            Collection? gettingColletion = await context
+                                                .Collections
+                                                .SingleOrDefaultAsync(c => c.Id.Equals(request.Id), cancellationToken);
+
+            if (gettingColletion is null) return Result.NotFound();
+            // check if user is the owner of the collection
+            if (gettingColletion.UserId != currentUser.User!.Id) return Result.Forbidden();
+            // soft delete collection
+            gettingColletion.Delete();
+            await context.SaveChangesAsync(cancellationToken);
+            return Result.NoContent();
+        }
+    }
+}
