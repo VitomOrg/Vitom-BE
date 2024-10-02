c9144e199c8c56cd047a2eb91323dbb6f13465ba
PersieDaCoder <thinhdpham2510@gmail.com>
2024-10-02 17:16:17 +0700

    fix:webhook payos

--- src/API/Extensions/PayOSExtension.cs
+++ src/API/Extensions/PayOSExtension.cs
@@ -14,23 +14,23 @@ public static class PayOSExtension
         string clientId = payOSSettings.ClientId;
         string apiKey = payOSSettings.ApiKey;
         string checkSumKey = payOSSettings.CheckSumKey;
-        PayOS payOS = new(clientId, apiKey, checkSumKey);
+        // PayOS payOS = new(clientId, apiKey, checkSumKey);
         Console.WriteLine("Client Id: " + clientId);
         Console.WriteLine("Api Key: " + apiKey);
         Console.WriteLine("Check Sum Key: " + checkSumKey);
         string webhook = "";
         try
         {
-            webhook = await payOS.confirmWebhook("https://vitom-api.persiehomeserver.com/payment/webhook/");
-            // HttpClient httpClient = new HttpClient();
-            // httpClient.DefaultRequestHeaders.Add("x-client-id", clientId);
-            // httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
-            // string url = "https://api-merchant.payos.vn/v2/payment-requests/";
-            // var response = await httpClient.PostAsync(url, new StringContent("{\"webhookUrl\": \"https://vitom-api.persiehomeserver.com/payment/webhook/\"}", Encoding.UTF8, "application/json"));
-            // if (response.IsSuccessStatusCode)
-            // {
-            //     webhook = await response.Content.ReadAsStringAsync();
-            // }
+            // webhook = await payOS.confirmWebhook("https://vitom-api.persiehomeserver.com/payment/webhook/");
+            HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("x-client-id", clientId);
+            httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+            string url = "https://api-merchant.payos.vn/v2/payment-requests/";
+            var response = await httpClient.PostAsync(url, new StringContent("{\"webhookUrl\": \"https://vitom-api.persiehomeserver.com/payment/webhook/\"}", Encoding.UTF8, "application/json"));
+            if (response.IsSuccessStatusCode)
+            {
+                webhook = await response.Content.ReadAsStringAsync();
+            }
         }
         catch (Exception ex)
         {
