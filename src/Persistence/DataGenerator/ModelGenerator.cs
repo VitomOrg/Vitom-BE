66c093f396e9ee5c11bdd03062c7515a7028cbb3
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-10-04 23:58:17 +0700

    add model generator add migrations

---
+++ src/Persistence/DataGenerator/ModelGenerator.cs
@@ -0,0 +1,22 @@
+using Bogus;
+using Domain.Entities;
+
+namespace Persistence.DataGenerator;
+
+public class ModelGenerator
+{
+    public static Model[] Generate(Product[] products)
+        => [..new Faker<Model>()
+            .UseSeed(1)
+            .UseDateTimeReference(DateTime.UtcNow)
+            //base  enity
+            .RuleFor(m => m.Glb, f => f.Image.LoremFlickrUrl())
+            .RuleFor(m => m.Obj, f => f.Image.LoremFlickrUrl())
+            .RuleFor(m => m.Fbx, f => f.Image.LoremFlickrUrl())
+            .RuleFor(m => m.ProductId, f => f.PickRandom(products).Id)
+            .Generate(100)
+            .ToArray()
+            .DistinctBy(m => m.ProductId)
+            .ToArray()
+        ];
+}
---
+++ src/Persistence/Migrations/20241004162120_AddIsLicenseInUseAndModelTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsLicenseInUseAndModelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLicense",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Fbx = table.Column<string>(type: "text", nullable: false),
+                    Obj = table.Column<string>(type: "text", nullable: false),
+                    Glb = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Models_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_ProductId",
+                table: "Models",
+                column: "ProductId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Models");
+
+            migrationBuilder.DropColumn(
+                name: "IsLicense",
+                table: "Users");
+        }
+    }
+}
