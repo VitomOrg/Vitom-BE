b8522679981530f3ceb89075c7a8f3351a230e59
PersieDaCoder <thinhdpham2510@gmail.com>
2024-11-02 12:13:55 +0700

    fix: clear data

--- src/Persistence/DataGenerator/SoftwareGenerator.cs
+++ src/Persistence/DataGenerator/SoftwareGenerator.cs
@@ -33,18 +33,9 @@ public class SoftwareGenerator
     "SelfCAD"
     ];
     public static Software[] Generate()
-        => [.. new Faker<Software>()
-            .UseSeed(1)
-            .UseDateTimeReference(DateTime.UtcNow)
-            // base entity
-            .RuleFor(e => e.Id, f => f.Random.Uuid())
-            .RuleFor(e => e.CreatedAt, f => f.Date.Past())
-            .RuleFor(e => e.UpdatedAt, f => f.Random.Bool() ? f.Date.Past() : null!)
-            .RuleFor(e => e.DeletedAt, (f, e) => f.Random.Bool() ? f.Date.Past() : null!)
-            .RuleFor(e => e.Name, f => f.PickRandom(softwareArray))
-            .RuleFor(e => e.Description, f => f.Lorem.Word())
-            .RuleFor(e => e.TotalPurchases, f => f.Random.Number(1, int.MaxValue))
-            .Generate(100)
-            .DistinctBy(e => new {e.Name})
-        ];
+        => softwareArray.Select(s => new Software
+        {
+            Name = s,
+            Description = $"Software {s} is a wonderful software"
+        }).ToArray();
 }
